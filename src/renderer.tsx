9925284cd42c153f52bb8eab103d9a7f1caf97ed
Nguyen Cong Cuong <c.ng.devie@gmail.com>
2024-09-19 19:33:45 +0700

    chore: setup Heroicons

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { BackwardIcon } from '@heroicons/react/16/solid';
 
 const App: React.FC = () => {
   return (
     <div>
       <div className={'bg-amber-200'}>
         Dozify - Keep Your Mac Awake, Effortlessly
+        <BackwardIcon className="h-6 w-6" />
       </div>
     </div>
   );
